317c66b862c0cbcec689f0973fcce10b142d8107
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-06-04 08:40:45 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-
-  public showPublisher:boolean;
-  constructor(public quote:string, public author:string, public publisher:string, public publishDate:Date, public upVote=0,public downVote=0 ){
-      this.showPublisher=false
-  }
-}
